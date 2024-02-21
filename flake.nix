399acf9b1bae6882dfdb2e8bc8524c43af579679
David Padlipsky <david.padlipsky@gmail.com>
2024-02-21 01:53:53 -0800

    Initial commit showing working repo w/ rust flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer pkg-config systemd dbus libxkbcommon];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
